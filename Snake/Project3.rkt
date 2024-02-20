7f60b97580c3d6446fa0d844254286449cee54f5
Yurii287 <124893639+Yurii287@users.noreply.github.com>
2024-02-20 13:29:24 +0000

    Add files via upload

--- Snake/Project3.rkt
+++ Snake/Project3.rkt
@@ -6,6 +6,19 @@
          "settings.rkt")
 
 ; Classes
+
+(define game-canvas%
+  (class canvas%
+    (define/override (on-char key-event)
+      (cond
+        ([equal? (send key-event get-key-code) 'up] (draw-square gameCanvas #(300 300)))
+        ([equal? (send key-event get-key-code) 'down] (draw-square gameCanvas #(200 200)))
+        ([equal? (send key-event get-key-code) 'left] (draw-square gameCanvas #(100 100)))
+        ([equal? (send key-event get-key-code) 'right] (draw-square gameCanvas #(50 50)))
+        )
+      )
+    (super-new)))
+
 (define snake%
   (class object%
     (super-new)
@@ -13,10 +26,13 @@
                 (body_coords (make-vector 0))
                 (body_squares (make-vector 0)) 
                 (body_color "green")
+                (direction "up")
                 )
     (define/public get-body-color (λ () body_color))
     (define/public get-body-coords (λ () body_coords))
     (define/public get-body-squares (λ () body_squares))
+    (define/public get-snake-direction (λ () direction))
+    (define/public set-snake-direction (λ (x) (set! direction x)))
     )
   )
 
@@ -35,7 +51,7 @@
 
 ; Functions
 (define rand-square (lambda ()
-                      (cons (random 0 (exact-round (/ (car FRAME_SIZE) SCALE))) (random 0 (exact-round (/ (cdr FRAME_SIZE) SCALE))))))
+                      (vector (* (random 0 (exact-round (/ (vector-ref FRAME_SIZE 0 ) SCALE))) SCALE) (* (random 0 (exact-round (/ (vector-ref FRAME_SIZE 1) SCALE))) SCALE))))
 
 (define draw-square (lambda (dc pos)
                       (let ([x (vector-ref pos 0)]
@@ -53,19 +69,17 @@
                        [stretchable-width #f]
                        [stretchable-height #f]))
 
-(define gameScreen (new canvas%
+(define gameScreen (new game-canvas%
                         [parent mainFrame]))
   
-
 ; Variables
 ; World variables
 (define gameCanvas (send gameScreen get-dc))
 ; Snake variables
 (define snake (new snake%))
-(define snake_brush (new brush% [color "green"]))
+(define snake_brush (new brush% [color (send snake get-body-color)]))
 (define snake_start (vector 0 0))
 ; Apple Variables
 (define apple (new apple%))
-(define apple_brush (new brush% [color "apple"]))
-
+(define apple_brush (new brush% [color (send apple get-apple-color)]))
 (send mainFrame show #t)
